41c74818238e96ee776862794595dc013edbfdeb
hyun907 <seunghyun020907@naver.com>
2024-11-30 19:39:03 +0900

    chore: add swr, zustand

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,27 @@
+import { Button } from "react-native";
 import { Text, View } from "react-native";
+// import { create } from "zustand";
+
+// const useStore = create(set => ({
+//   bears: 0,
+//   increasePopulation: () => set((state: any) => ({ bears: state.bears + 1 })),
+//   removeAllBears: () => set({ bears: 0 }),
+//   updateBears: (newBears: any) => set({ bears: newBears })
+// }));
+
+// function BearCounter() {
+//   const bears = useStore((state: any) => state.bears);
+//   return <Text>{bears} around here...</Text>;
+// }
 
 export default function HomeScreen() {
+  // const increasePopulation = useStore((state: any) => state.increasePopulation);
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
+      <Text>Zustand</Text>
+      {/* <Button title="버튼" onPress={increasePopulation} /> */}
+      {/* <BearCounter /> */}
     </View>
   );
 }
