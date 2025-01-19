4770f4a2bbf21abacda305fc2ced1b86bb822e13
widse <wngud6012@naver.com>
2025-01-20 00:22:08 +0900

    chore: build error test (jest)

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,7 @@ module.exports = {
   preset: "jest-expo",
   setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
   transform: {
-    "\\.[jt]sx?$": "babel-jest",
-    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "babel-jest"
+    "\\.[jt]sx?$": "babel-jest"
   },
   testPathIgnorePatterns: ["/node_modules/", "/android/", "/ios/"],
   transformIgnorePatterns: [],
